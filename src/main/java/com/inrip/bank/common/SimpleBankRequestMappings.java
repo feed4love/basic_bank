7bfba9e37d7c452483a1c7b3de8c7573a9ab9872
feed4love <eacaja@gmail.com>
2022-09-22 17:27:31 +0200

    refactor

--- src/main/java/com/inrip/bank/common/SimpleBankRequestMappings.java
+++ src/main/java/com/inrip/bank/common/SimpleBankRequestMappings.java
@@ -4,15 +4,22 @@ package com.inrip.bank.common;
  * @author Enrique AC
  *
  */
-public interface SimpleBankRequestMappings {	
+public interface SimpleBankRequestMappings {
+
+	//parent path mounted after the servlet context
 	public static String REQUEST_CONTEXT = "/account";
+
+	//path hello world
 	public static String SERVICE_STATUS = "/";	
+
+	//path transacciones
 	public static String SEARCH_BY_ACCOUNT_IBAN = "/transaction/iban/{account_iban}";	
-	public static String ADD_TRANSACTION = "/transaction/add";
-	public static String TRANSACTION_STATUS = "/transaction/status";
+	public static String ADD_TRANSACTION        = "/transaction/add";
+	public static String TRANSACTION_STATUS     = "/transaction/status";
 
 	//solo debug
-	public static String LIST_ALL = "/transaction/all";	
-	public static String SEARCH_BY_REFERENCE = "/transaction/reference/{reference}";
 	public static String SEARCH_ACCOUNT_BY_IBAN = "/{account_iban}";
+	public static String LIST_ALL               = "/transaction/all";	
+	public static String SEARCH_BY_REFERENCE    = "/transaction/reference/{reference}";
+	
 }
